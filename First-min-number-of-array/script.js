27f05c2e7feb8f1cec2d953bfdbe70764664b57c
volder <vlodyk00@gmail.com>
2020-05-21 12:26:42 +0300

    refactored

--- First-min-number-of-array/script.js
+++ First-min-number-of-array/script.js
@@ -1,7 +1,7 @@
-const minArr = arr => {
+const removeSmallest = arr => {
     let newArr = arr.map(el => el);
     let smallestNumb = newArr.indexOf(Math.min(...newArr));
     newArr.splice(smallestNumb, 1);
     return newArr;
 }
-console.log(minArr([3,0,3,1,2,3,4,1]));
+console.log(removeSmallest([2,3,1,2,3,4]));
