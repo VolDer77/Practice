5925a1d420962c1cf3a3f3368972cea92a20fd2c
Volodymyr Derkach <56925437+VolDer77@users.noreply.github.com>
2020-08-13 13:55:34 +0300

    new task added

---
+++ Unique in order/script.js
@@ -0,0 +1,10 @@
+function uniqueInOrder(str) {
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[i + 1]) {
+            result.push(str[i])
+        }
+    }
+    return result;
+}
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
